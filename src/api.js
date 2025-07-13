3b216bf9edb58af6c236abfc99064ac3af5c4ed7
Viacheslav <east.slava@gmail.com>
2025-07-13 22:35:30 +1200

    chage api.js to global server

--- src/api.js
+++ src/api.js
@@ -4,8 +4,8 @@ import { ACCESS_TOKEN } from './constants'
 
 const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8000/";
-//console.log(API_URL);
+const API_URL = "https://new-proj2.onrender.com/"//process.env.REACT_APP_API_URL || "http://localhost:8000/";
+console.log(API_URL);
 
 const api = axios.create({
   baseURL: API_URL ? API_URL : apiUrl,
